7725f63fdf0029910e6192a5c425093ac111842b
Jan Zípek <jan@zipek.cz>
2017-05-17 11:31:44 +0200

    Better docs for some BIS functions.

--- client/fetch-docs.js
+++ client/fetch-docs.js
@@ -41,6 +41,7 @@ function removeMoreTags(value) {
 		return value;
 	
 	return value
+		.replace(/<br>/ig, '')
 		.replace(/''\(since \[.*?\]\)''/ig, '')
 		.replace(/\(''since .*?''\)/ig, '')
 		.replace(/<nowiki>(.*?)<\/nowiki>/g, '$1')
@@ -51,12 +52,28 @@ function removeMoreTags(value) {
 		.replace(/&nbsp;/g, '');
 }
 
+function polishSyntaxArgument(item) {
+	if (item.desc.length < 10) {
+		return item.desc;
+	}
+	if (item.type.length > 10) {
+		return item.type.substr(0, 8) + "..";
+	}
+	return item.type;
+}
+
 function parseDocument(doc, type, extended) {
 	var document = new xmldoc.XmlDocument(doc);
 	var pages = document.childrenNamed("page");
 	var findCommand = /{{(?:Command|Function)\|=((?:.|\n)*?)}}/i;
 	var findVariable = /\|\s*((?:.|\n)*?)\s*\|\s*=\s*(.*)/g;
 	var results = extended || {};
+	var placeHolder = /\/\*\s*Description:\s*([^]*)\s*(?:Parameters|Parameter\(s\)):\s*([^]*)\s*Returns:\s*([^]*)\s*\*\/\s*/i;
+	var placeHolderWithExamples = /\/\*\s*Description:\s*([^]*)\s*(?:Parameters|Parameter\(s\)):\s*([^]*)\s*Returns:\s*([^]*)\s*Examples:\s*([^]*)\s*\*\/\s*/i;
+	var placeHolderArgument = /([0-9]+):\s*([^-]*)-\s*(.*)/g;
+	var placeHolderSingularArgument = /_this:\s*([^-]*)-\s*(.*)/i;
+	var placeHolderReturn = /\s*([^-]*)(.*)/;
+	var sqfTypes = ["bool", "string", "object", "task", "array", "scalar", "number", "side", "group", "boolean", "code", "config", "control", "display", "namespace"];
 
 	for(var i in pages) {
 		var page = pages[i];
@@ -81,7 +98,7 @@ function parseDocument(doc, type, extended) {
 				var returns = [];
 				var syntax = [];
 				
-				while(match = findVariable.exec(wiki)) {
+				while (match = findVariable.exec(wiki)) {
 					var ident = match[2].toLowerCase().trim();
 					var value = match[1];
 
@@ -102,21 +119,80 @@ function parseDocument(doc, type, extended) {
 				if (variables["syntax"])
 					syntax = variables["syntax"];
 
+				syntax = syntax.map((item) => {
+					var commented = /<!--\s*(\[\]\s*call\s*[^;]+);\s*-->/i.exec(item);
+					if (commented) {
+						item = commented[1].trim();
+					}
+					return item;
+				});
+
 				// Parse description
 				if (variables["description"]) {
 					var desc = variables["description"];
+					match = placeHolderWithExamples.exec(desc);
+					if (!match) {
+						match = placeHolder.exec(desc);
+					}
 
-					// Remove image tags
-					desc = desc.replace(/\[\[Image(.*?)\]\]/g, '');
+					if (match) {
+						desc = match[1];
 
-					// Only use first sentence
-					var dot = desc.indexOf(".");
-					if (dot >= 0) {
-						desc = desc.substr(0, dot + 1);
+						if (match[2].trim()) {
+							var args = [];
+							var amatch = null;
+							while (amatch = placeHolderArgument.exec(match[2])) {
+								args.push({
+									type: amatch[2].trim().replace(/(\s+)or(\s+)/g, "/"),
+									desc: amatch[3].trim()
+								});
+							}
+
+							if (args.length > 0) {
+								syntax = ["[" + args.map((item) => polishSyntaxArgument(item)).join(", ") + "] call " + title];
+
+								desc += "\r\nArguments:\r\n";
+								args.forEach((arg, index) => {
+									// desc += "\r\n - " + (index + 1) + ": " + arg.type + " - " + arg.desc
+									desc += "\r\n " + index + ". " + arg.type + " - " + arg.desc;
+								});
+							} else {
+								amatch = placeHolderSingularArgument.exec(match[2]);
+								if (amatch) {
+									var arg = { type: amatch[1].trim(), desc: amatch[2].trim() };
+									syntax = [polishSyntaxArgument(arg) + " call " + title];
+									desc += "\r\nArgument: _this: " + arg.type + " - " + arg.desc;
+								}
+							}
+						}
+
+						if (match[3].trim()) {
+							match[3] = match[3].trim();
+							var rmatch = placeHolderReturn.exec(match[3]);
+							if (rmatch) {
+								returns = [rmatch[1]];
+								desc += "\r\n\r\nReturns: " + match[3];
+							} else if (sqfTypes.indexOf(match[3].toLowerCase()) != -1) {
+								returns = [match[3]];
+							} else {
+								returns = ["ANY"];
+								desc += "\r\n\r\nReturns: " + match[3];
+							}
+						}
+					} else {
+						// Remove image tags
+						desc = desc.replace(/\[\[Image(.*?)\]\]/g, '');
+
+						// Only use first sentence
+						var dot = desc.indexOf(".");
+						if (dot >= 0) {
+							desc = desc.substr(0, dot + 1);
+						}
 					}
 
 					description.plain = fixSyntax(desc).trim();
 					description.formatted = desc.replace(/'''(.*?)'''/g, '**$1**')
+						.replace(/<br>/ig, '')
 						.replace(/\[\[(.*?)\|(.*?)\]\]/g, '[$1](https://community.bistudio.com/wiki/$2)')
 						.replace(/\[\[(.*?)\]\]/g, '[$1](https://community.bistudio.com/wiki/$1)')
 						.trim();
