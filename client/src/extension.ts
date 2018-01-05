e606a155b9e806bc2bd60274e43a63bbfcfb9e6a
Jan Zípek <jan@zipek.cz>
2018-01-05 22:59:04 +0100

    Updated extension to use newer debugging api

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -10,21 +10,12 @@ import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 
 import * as openurl from 'openurl';
-
-const initialConfigurations = {
-	version: '0.2.0',
-	configurations: [
-	{
-		type: 'sqflint',
-		request: 'launch',
-		name: 'SQFLint',
-		rptPath: path.join(process.env.LOCALAPPDATA, 'Arma 3')
-	}
-]}
+import { CancellationToken } from 'vscode-languageclient/node_modules/vscode-jsonrpc/lib/main';
+import { ProviderResult, DebugConfiguration } from 'vscode';
 
 const links = {
-    unitEventHandlers: "https://community.bistudio.com/wiki/Arma_3:_Event_Handlers",
-    uiEventHandlers: "https://community.bistudio.com/wiki/User_Interface_Event_Handlers",
+	unitEventHandlers: "https://community.bistudio.com/wiki/Arma_3:_Event_Handlers",
+	uiEventHandlers: "https://community.bistudio.com/wiki/User_Interface_Event_Handlers",
 	commandsList: "https://community.bistudio.com/wiki/Category:Scripting_Commands"
 }
 
@@ -33,14 +24,14 @@ export function activate(context: vscode.ExtensionContext) {
 	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
 	// The debug options for the server
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
-	
+
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
 		run : { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
 	};
-	
+
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		documentSelector: ['sqf', 'ext', 'hpp'],
@@ -48,14 +39,6 @@ export function activate(context: vscode.ExtensionContext) {
 			configurationSection: 'sqflint'
 		}
 	};
-
-	context.subscriptions.push(vscode.commands.registerCommand('extension.sqflint.provideInitialConfigurations', () => {
-		return [
-			'// Use IntelliSense to learn about possible debug attributes.',
-			'// Hover to view descriptions of existing attributes.',
-			JSON.stringify(initialConfigurations, null, '\t')
-		].join('\n');
-	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.sqflint.unitEvents', () => {
 		openurl.open(links.unitEventHandlers);
@@ -68,11 +51,11 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('extension.sqflint.commandsList', () => {
 		openurl.open(links.commandsList);
 	}));
-	
+
 	// Create the language client and start the client.
 	let disposable = new LanguageClient('SQF Language Server', serverOptions, clientOptions).start();
-	
-	// Push the disposable to the context's subscriptions so that the 
+
+	// Push the disposable to the context's subscriptions so that the
 	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
 }
