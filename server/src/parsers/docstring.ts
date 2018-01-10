0a4c1aaf695fceb7f0da5ca21c48d1b105912782
Jan Zípek <jan@zipek.cz>
2018-01-10 10:52:23 +0100

    Rewritten docstring parser to be more flexible

--- server/src/parsers/docstring.ts
+++ server/src/parsers/docstring.ts
@@ -27,15 +27,6 @@ export namespace Docstring {
 	// Used for cleanup, newline and tabs
 	const tabRegex = /\n\t*/ig;
 
-	// Start of description section
-	const descRegex = /description:(?:\s|\n|\r)*(.*)/i;
-
-	// Start of parameters section, 1 = contents after parameters tag
-	const paramsRegex = /parameter(?:\(s\)|s)?:([^]*)/im;
-
-	// Start of returns section
-	const returnRegex = /returns:(?:\s|\n|\r)*(.*)/i;
-
 	// Matches param, 1 = index, 2 = optional, 3 = type, 4 = description
 	const bisParam = /(?:_this\s*select)?\s*([0-9]+)\s*(\(optional\))?\s*:\s*(\w*)\s*-\s*(.*)/i;
 
@@ -44,6 +35,16 @@ export namespace Docstring {
 
 	// Matches returns description with type, 1 = type, 2 = description
 	const returnWithDesc = /([^-:]*)[-:](.*)/;
+
+	// Matches section beginning, 1 = section name, 2 = rest of the line
+	const sectionRegex = /^\s*([a-z\(\)]+):\s*(.*)/i;
+
+	// Sections in docstring
+	type SECTION = 'returns' | 'parameters' | 'description' | 'author';
+	const SECTION_RETURNS: SECTION = 'returns';
+	const SECTION_PARAMETERS: SECTION = 'parameters';
+	const SECTION_DESCRIPTION: SECTION = 'description';
+	const SECTION_AUTHOR: SECTION = 'author';
 
 	/**
 	 * Prepares docstring to be easier to parse.
@@ -55,6 +56,11 @@ export namespace Docstring {
 		return comment.trim().replace(tabRegex, "\n");
 	}
 
+	/**
+	 * Parses docstring comment into structured output.
+	 *
+	 * @param comment docstring comment contents (without /*)
+	 */
 	export function parse (comment: string) {
 		// Initialize result object
 		let result: Info = {
@@ -72,66 +78,121 @@ export namespace Docstring {
 			parameter: null
 		};
 
-		comment = preprocess(comment);
+		let section: SECTION = null;
+		let match: RegExpMatchArray = null;
 
-		// Try to load description
-		let match = descRegex.exec(comment);
-		if (match) {
-			result.description.short = match[1].trim().replace(/(\r?\n)/g, '$1$1');
-		}
+		preprocess(comment)
+			.split("\n")
+			.forEach(line => {
+				line = line.trim();
 
-		// Try to load return type
-		match = returnRegex.exec(comment);
-		if (match) {
-			let value = match[1].trim();
-
-			match = returnWithDesc.exec(value)
-			if (match) {
-				result.returns.type = match[1].trim();
-				result.returns.description = match[2].trim();
-			} else {
-				result.returns.type = value.split(' ').shift();
-			}
-		}
-
-		// Try to load params
-		match = paramsRegex.exec(comment);
-		if (match) {
-			let lines = match[1].trim().split('\n');
-
-			lines.forEach(line => {
-				if (line.trim().length === 0) return;
-
-				console.log('Checking line', line);
-
-				let param: InfoParamater = {
-					name: null,
-					type: null,
-					description: null,
-					optional: null,
-					default: null
-				};
-
-				let match = bisParam.exec(line);
+				// Switch section if needed
+				match = sectionRegex.exec(line);
 				if (match) {
-					param.optional = !!match[2];
-					param.type = match[3];
-					param.description = match[4];
+					let ident = match[1].trim().toLowerCase();
+					let unknown = false;
+					let previous = section;
 
-					result.parameters.push(param);
+					switch (ident) {
+						case 'returns':
+						case 'return':
+							section = SECTION_RETURNS;
+							break;
+						case 'description':
+							section = SECTION_DESCRIPTION;
+							break;
+						case 'author':
+							section = SECTION_AUTHOR;
+							break;
+						case 'parameter':
+						case 'parameters':
+						case 'parameter(s)':
+							section = SECTION_PARAMETERS;
+							break;
+						default:
+							unknown = true;
+							break;
+					}
+
+					// Section was recognized
+					if (!unknown) {
+						// Some sections can have data at same line
+						line = match[2].trim();
+
+						// If there are no data, continue to next line
+						if (!line) return;
+					}
 				}
 
-				match = !match && bisParamSingular.exec(line);
-				if (match) {
-					param.type = match[1];
-					param.description = match[2];
+				switch (section) {
+					case SECTION_AUTHOR:
+						if (!result.author) {
+							result.author = "";
+						} else {
+							result.author += "\n";
+						}
 
-					result.parameter = param;
+						result.author += line;
+						break;
+
+					case SECTION_DESCRIPTION:
+						if (!result.description.full) {
+							result.description.full = "";
+							result.description.short = line.trim().replace(/(\r?\n)/g, '$1$1');
+						} else {
+							result.description.full += "\n";
+						}
+
+						result.description.full += line;
+						break;
+
+					case SECTION_RETURNS:
+						// Try to separate type and description
+						match = returnWithDesc.exec(line)
+						if (match) {
+							result.returns.type = match[1].trim();
+							result.returns.description = match[2].trim();
+						} else {
+							// Use first word, which should be type
+							result.returns.type = line.split(' ').shift();
+						}
+						break;
+
+					case SECTION_PARAMETERS:
+						// Skip empty lines
+						if (!line) return;
+
+						// Prepare param struct
+						let param: InfoParamater = {
+							name: null,
+							type: null,
+							description: null,
+							optional: null,
+							default: null
+						};
+
+						// Try classic BIS param declaration
+						match = bisParam.exec(line);
+						if (match) {
+							param.optional = !!match[2];
+							param.type = match[3];
+							param.description = match[4];
+
+							result.parameters.push(param);
+						}
+
+						// Try BIS singular param declaration
+						match = !match && bisParamSingular.exec(line);
+						if (match) {
+							param.type = match[1];
+							param.description = match[2];
+
+							result.parameter = param;
+						}
+
+						break;
 				}
 			})
-		}
-
-		console.log(result)
 
 		return result;
 	}
