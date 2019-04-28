325a01097209d7a08f870abb4fdecbae28a1942a
Jan Zipek <jan@zipek.cz>
2019-04-28 14:36:32 +0200

    Removed unused imports.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -10,8 +10,6 @@ import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 
 import * as openurl from 'openurl';
-import { CancellationToken } from 'vscode-languageclient/node_modules/vscode-jsonrpc/lib/main';
-import { ProviderResult, DebugConfiguration } from 'vscode';
 
 const links = {
 	unitEventHandlers: "https://community.bistudio.com/wiki/Arma_3:_Event_Handlers",
