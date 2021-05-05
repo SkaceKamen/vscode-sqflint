a980896f4b7c37dd92ae2a7ab4cb4a7454592390
Jan Zípek <jan@zipek.cz>
2021-05-05 11:38:04 +0200

    Fixed issues with non-ascii file names

--- server/src/java.ts
+++ server/src/java.ts
@@ -3,6 +3,6 @@ import * as child from 'child_process';
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace Java {
     export function spawn(jar: string, args: string[]): child.ChildProcess {
-        return child.spawn("java", [ "-jar", jar ].concat(args));
+        return child.spawn("java", [ "-Dfile.encoding=UTF-8", "-jar", jar ].concat(args));
     }
 }
